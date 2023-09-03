b0e04db0bd12bfc94bb46534894c13315cfe9a55
mehedimk <mehedikhan.cse@gmail.com>
2023-09-04 04:49:15 +0600

    Optimize code & prepare for reuse

--- scripts/services/service.py
+++ scripts/services/service.py
@@ -1,33 +1,34 @@
 import requests
-from scripts import config
 import scripts as scripts_app
 from blog import models
-from django.contrib.auth import get_user_model
 
 
 class Service:
-    def insert_data_from_api(self, username, api_type:str='all'):
+    def insert_data_from_api(self, api_type:str='all'):
         if api_type:
             pass
         url = scripts_app.POST_API
         response = requests.get(url)
-        User = get_user_model()
-        user = User.objects.get(username=username)
-        if response.status_code == 200:
-            object_list = []
+        if response.status_code == 200:            
             datas = response.json()
-            for data in datas:                
-                payload = {}
-                payload['slug'] = data.get('slug'),
-                payload['url'] = data.get('url'),
-                payload['title'] = data.get('title'),
-                payload['content'] = data.get('content'),
-                payload['post_image_url'] = data.get('image'),
-                payload['post_thumbnail_url'] = data.get('thumbnail'),
-                payload['status'] = data.get('status'),
-                payload['category'] = data.get('category')
-                object_list.append(payload)
+            object_list = self.make_post_model_paylad(datas)            
             self.bulk_insert(object_list)
+    
+    def make_post_model_paylad(self, datas):
+        object_list = []
+        for data in datas:                
+            payload = {}
+            payload['slug'] = data.get('slug'),
+            payload['url'] = data.get('url'),
+            payload['title'] = data.get('title'),
+            payload['content'] = data.get('content'),
+            payload['post_image_url'] = data.get('image'),
+            payload['post_thumbnail_url'] = data.get('thumbnail'),
+            payload['status'] = data.get('status'),
+            payload['category'] = data.get('category')
+            
+            object_list.append(payload)
+        return object_list
 
 
     def bulk_insert(self, datas):
